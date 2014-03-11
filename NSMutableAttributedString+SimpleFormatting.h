223ca1acbbc2b883bb12e52ad09facc12bb0dcf8
George MacKerron <george@mackerron.co.uk>
2014-03-11 14:07:17 +0000

    Update NSMutableAttributedString+SimpleFormatting.h

--- NSMutableAttributedString+SimpleFormatting.h
+++ NSMutableAttributedString+SimpleFormatting.h
@@ -1,13 +1,35 @@
-@interface NSMutableAttributedString (SimpleFormatting)
+//
+//  NSMutableAttributedString+InlineStyles.h
+//  https://github.com/jawj/NSMutableAttributedString-InlineStyles
+//
+//  Created by George MacKerron on 11/03/2014.
+//  Copyright (c) 2014 George MacKerron. MIT-licenced.
+//
 
-+ (NSMutableAttributedString*)fromString:(NSString*)markdown
-                        withFontBaseName:(NSString*)fontName
-                                    size:(CGFloat)size;
+#import <Foundation/Foundation.h>
 
-+ (NSMutableAttributedString*)fromString:(NSString*)markdown
-                         withRegularFont:(UIFont*)regularFont
-                                boldFont:(UIFont*)boldFont
-                              italicFont:(UIFont*)italicFont
-                          boldItalicFont:(UIFont*)boldItalicFont;
+@interface NSMutableAttributedString (InlineStyles)
+
+- (void)applyFormattingWithFontSize:(CGFloat)fontSize
+                 andNamesForRegular:(NSString*)regularFontName
+                               bold:(NSString*)boldFontName
+                             italic:(NSString*)italicFontName
+                         boldItalic:(NSString*)boldItalicFontName;
+
+#pragma mark Convenience methods
+
+- (void)applyFormattingWithFontSize:(CGFloat)fontSize
+                        andBaseName:(NSString*)fontBaseName;
+
++ (NSMutableAttributedString*)withString:(NSString*)str
+                            fontBaseName:(NSString*)fontBaseName
+                                 andSize:(CGFloat)fontSize;
+
++ (NSMutableAttributedString*)withString:(NSString*)str
+                     fontNamesForRegular:(NSString*)regularFontName
+                                    bold:(NSString*)boldFontName
+                                  italic:(NSString*)italicFontName
+                              boldItalic:(NSString*)boldItalicFontName
+                                 andSize:(CGFloat)fontSize;
 
 @end
